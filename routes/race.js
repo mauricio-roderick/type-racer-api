c140963502f17bac8fcbd6b4554bcbc841da3a0f
Erick <roderick.mauricio@headhuntr.io>
2021-03-27 20:16:41 +0800

    Initial race route

--- routes/race.js
+++ routes/race.js
@@ -1,9 +1,43 @@
 const router = require('express').Router()
+const db = require("../database")
+const _ = require('lodash')
+const moment = require('moment')
+const uuidV4 = require('uuid').v4
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
+  const { page } = req.query
+  let { limit } = req.query
+  
+  if (!limit || limit > 20) limit = 20
+
+  const filter = {
+    user: req.user._id
+  }
+  const totalRecords = await db.race.count(filter)
+  const data = await db.race
+    .find(filter)
+    .sort({ timestamp: -1 })
+    .skip(page * limit)
+    .limit(limit)
+
   res.status(200).send({
-    message: 'Type Racer API now running.'
+    race: data,
+    meta: {
+      totalRecords
+    }
   })
+})
+
+router.post('/', async (req, res, next) => {
+  const data = _.pick(req.body, ['wpm', 'time', 'textLength'])
+  const doc = await db.race.insert({
+    ...data,
+    _id: uuidV4(),
+    user: req.user._id,
+    timestamp: moment().format()
+  })
+
+  res.status(201).send(doc)
 })
 
 module.exports = router
