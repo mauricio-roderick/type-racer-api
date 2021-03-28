0cfef29ba6311e10c20d4c0717c650c19628b749
Erick <roderick.mauricio@headhuntr.io>
2021-03-28 16:28:48 +0800

    Remove health check endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,6 @@ const Chance = require('chance')
 const chance = new Chance()
 const maxWords = 100
 
-router.get('/', (req, res, next) => {
-  res.status(200).send({
-    message: 'Type Racer API now running.'
-  })
-})
-
 router.get('/random-text', (req, res, next) => {
   let { words } = req.query
 
