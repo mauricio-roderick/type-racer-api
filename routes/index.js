bc09867920a3cf9063e10aa0873cf6a79562531b
Erick <roderick.mauricio@headhuntr.io>
2021-03-28 10:06:57 +0800

    Random text generator endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 const router = require('express').Router()
+const Chance = require('chance')
+
+const chance = new Chance()
+const maxWords = 100
 
 router.get('/', (req, res, next) => {
   res.status(200).send({
     message: 'Type Racer API now running.'
+  })
+})
+
+router.get('/random-text', (req, res, next) => {
+  let { words } = req.query
+
+  if (isNaN(words) || words > maxWords) {
+    words = maxWords
+  }
+
+  res.status(200).send({
+    text: chance.sentence({ words })
   })
 })
 
