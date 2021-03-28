3aadc3ed87f7cd22c6265ac329d4e3103a85e877
Erick <roderick.mauricio@headhuntr.io>
2021-03-28 08:42:56 +0800

    Race history db index

--- database/index.js
+++ database/index.js
@@ -2,6 +2,8 @@ const Datastore = require('nedb-promises')
 const user = new Datastore({ filename: './database/user.db' })
 const raceHistory = new Datastore({ filename: './database/race-history.db' })
 
+raceHistory.ensureIndex({ fieldName: 'timestamp'})
+
 const db = {
   user,
   raceHistory
